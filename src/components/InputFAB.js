c30220ade8f5572e52460ad219a2b3063bca97df
Roy Kang <dv.rakry@gmail.com>
2025-04-07 11:40:50 +0900

    6.11 추가 기능 만들기 부분 text.trim() 오류발생

---
+++ src/components/InputFAB.js
@@ -0,0 +1,174 @@
+import {
+  Animated,
+  Keyboard,
+  Platform,
+  Pressable,
+  StyleSheet,
+  TextInput,
+  useWindowDimensions,
+} from 'react-native';
+import { BLACK, PRIMARY, WHITE } from '../colors';
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
+import { useEffect, useRef, useState } from 'react';
+import PropTypes from 'prop-types';
+
+const BOTTOM = 30;
+const BUTTON_WIDTH = 60;
+
+const InputFAB = ({ onInsert }) => {
+  const [text, setText] = useState('');
+  const [isOpened, setIsOpened] = useState(false);
+  const inputRef = useRef(null);
+  const windowWidth = useWindowDimensions().width;
+  const [keyboardHeight, setKeyboardHeight] = useState(BOTTOM);
+
+  const inputWidth = useRef(new Animated.Value(BUTTON_WIDTH)).current;
+  const buttonRotation = useRef(new Animated.Value(0)).current;
+  const spin = buttonRotation.interpolate({
+    inputRange: [0, 1],
+    outputRange: ['0deg', '315deg'],
+  });
+
+  const open = () => {
+    setIsOpened(true);
+    Animated.timing(inputWidth, {
+      toValue: windowWidth - 20,
+      useNativeDriver: false,
+      duration: 300,
+    }).start(() => {
+      inputRef.current.focus();
+    });
+    Animated.spring(buttonRotation, {
+      toValue: 1,
+      useNativeDriver: false,
+      bounciness: 20,
+    }).start();
+  };
+
+  const close = () => {
+    setIsOpened(false);
+    Animated.timing(inputWidth, {
+      toValue: BUTTON_WIDTH,
+      useNativeDriver: false,
+      duration: 300,
+    }).start(() => {
+      inputRef.current.blur();
+    });
+    Animated.spring(buttonRotation, {
+      toValue: 0,
+      useNativeDriver: false,
+      bounciness: 20,
+    }).start();
+  };
+
+  const onPressButton = () => (isOpened ? close() : open());
+
+  const onPressInsert = () => {
+    const task = text.trim();
+    if (task) {
+      onInsert(task);
+    }
+  };
+
+  useEffect(() => {
+    if (Platform.OS === 'ios') {
+      const show = Keyboard.addListener('keyboardWillShow', (e) => {
+        setKeyboardHeight(e.endCoordinates.height + BOTTOM);
+      });
+
+      const hide = Keyboard.addListener('keyboardWillHide', () => {
+        setKeyboardHeight(BOTTOM);
+      });
+
+      return () => {
+        show.remove();
+        hide.remove();
+      };
+    }
+  }, []);
+
+  return (
+    <>
+      <Animated.View
+        style={[
+          styles.container,
+          styles.shadow,
+          {
+            bottom: keyboardHeight,
+            alignItems: 'flex-start',
+            width: inputWidth,
+          },
+        ]}
+      >
+        <TextInput
+          ref={inputRef}
+          value={text}
+          onChange={setText}
+          style={styles.input}
+          autoCapitalize={'none'}
+          autoCorrect={false}
+          textContentType={'none'}
+          keyboardAppearance={'light'}
+          returnKeyType={'done'}
+          onBlur={close}
+          onSubmitEditing={onPressInsert}
+        />
+      </Animated.View>
+
+      <Animated.View
+        style={[
+          styles.container,
+          { bottom: keyboardHeight, transform: [{ rotate: spin }] },
+        ]}
+      >
+        <Pressable
+          onPress={onPressButton}
+          style={({ pressed }) => [
+            styles.container,
+            { right: 0 },
+            pressed && { backgroundColor: PRIMARY.DARK },
+          ]}
+        >
+          <MaterialCommunityIcons name="plus" size={24} color={WHITE} />
+        </Pressable>
+      </Animated.View>
+    </>
+  );
+};
+
+InputFAB.propTypes = {
+  onInsert: PropTypes.func.isRequired,
+};
+
+const styles = StyleSheet.create({
+  container: {
+    position: 'absolute',
+    right: 10,
+    width: BUTTON_WIDTH,
+    height: BUTTON_WIDTH,
+    borderRadius: BUTTON_WIDTH / 2,
+    backgroundColor: PRIMARY.DEFAULT,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  input: {
+    color: WHITE,
+    paddingLeft: 20,
+    paddingRight: BUTTON_WIDTH + 10,
+  },
+  shadow: {
+    shadowColor: BLACK,
+    ...Platform.select({
+      ios: {
+        shadowOffset: { width: 2, height: 4 },
+        shadowOpacity: 0.5,
+        shadowRadius: 5,
+      },
+      android: {
+        elevation: 5,
+      },
+    }),
+  },
+});
+
+export default InputFAB;
--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -1,10 +1,25 @@
+import { useState } from 'react';
 import EmptyList from '../components/EmptyList';
+import InputFAB from '../components/InputFAB';
 import List from '../components/List';
+import { nanoid } from 'nanoid';
+import 'react-native-get-random-values';
 
 const ListScreen = () => {
-  const todos = [];
+  const [todos, setTodos] = useState([]);
 
-  return todos.length ? <List /> : <EmptyList />;
+  const onInsert = (task) => {
+    const id = nanoid();
+    const newTask = { id, task, isDone: false };
+    setTodos((prev) => [newTask, ...prev]);
+  };
+
+  return (
+    <>
+      {todos.length ? <List data={todos} /> : <EmptyList />}
+      <InputFAB onInsert={onInsert} />
+    </>
+  );
 };
 
 export default ListScreen;
